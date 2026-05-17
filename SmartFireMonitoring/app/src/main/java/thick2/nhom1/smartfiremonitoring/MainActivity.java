5e7bd002e997845c4187635662cacd2733de3270
Tran Mai Ngoc Duy <tmnduy.it@gmail.com>
2026-05-17 11:39:59 +0700

    feat: add bottom navigation with 5 fragments and navigation handling

--- SmartFireMonitoring/app/src/main/java/thick2/nhom1/smartfiremonitoring/MainActivity.java
+++ SmartFireMonitoring/app/src/main/java/thick2/nhom1/smartfiremonitoring/MainActivity.java
@@ -9,10 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,10 +55,41 @@ public class MainActivity extends AppCompatActivity {
                             Toast.LENGTH_SHORT
                     ).show();
                 });
+        bottomNav = findViewById(R.id.bottomNav);
+        // Mặc định
+        loadFragment(new DashboardFragment());
+
+        bottomNav.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.nav_dashboard) {
+                loadFragment(new DashboardFragment());
+
+            } else if (item.getItemId() == R.id.nav_history) {
+                loadFragment(new HistoryFragment());
+
+            } else if (item.getItemId() == R.id.nav_threshold) {
+                loadFragment(new ThresholdFragment());
+
+            } else if (item.getItemId() == R.id.nav_alert) {
+                loadFragment(new NotificationFragment());
+
+            } else if (item.getItemId() == R.id.nav_control) {
+                loadFragment(new ControlFragment());
+            }
+
+            return true;
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 }
