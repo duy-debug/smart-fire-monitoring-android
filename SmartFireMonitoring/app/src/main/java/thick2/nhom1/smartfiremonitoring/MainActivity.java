a4ae8b6dfef9b5f3fb1d49ad70e5c661c5d044d8
Tran Mai Ngoc Duy <tmnduy.it@gmail.com>
2026-05-17 10:50:57 +0700

    feat(firebase): integrate Firebase Realtime Database and anonymous authentication

--- SmartFireMonitoring/app/src/main/java/thick2/nhom1/smartfiremonitoring/MainActivity.java
+++ SmartFireMonitoring/app/src/main/java/thick2/nhom1/smartfiremonitoring/MainActivity.java
@@ -1,6 +1,8 @@
 package thick2.nhom1.smartfiremonitoring;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +10,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,6 +19,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        // Bật offline persistence
+        FirebaseHelper.enableOfflineMode();
+
+        // Đăng nhập ẩn danh Firebase
+        FirebaseAuth.getInstance()
+                .signInAnonymously()
+                .addOnSuccessListener(result -> {
+
+                    Log.d("FIREBASE_AUTH", "Login success");
+
+                    Toast.makeText(
+                            this,
+                            "Firebase Connected",
+                            Toast.LENGTH_SHORT
+                    ).show();
+
+                    // TODO:
+                    // bắt đầu đọc dữ liệu Firebase ở đây
+
+                })
+                .addOnFailureListener(e -> {
+
+                    Log.e(
+                            "FIREBASE_AUTH",
+                            e.getMessage()
+                    );
+
+                    Toast.makeText(
+                            this,
+                            "Login Failed",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
